21bb98c1b47df68ca22f36bd3d38e86009c55718
lwatson2 <watson.logan@yahoo.com>
2019-08-16 22:22:40 -0500

    Continued working on navbar styling for desktop

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,17 +15,17 @@ const Header = ({ siteTitle }) => {
           </h2>
           <button className="nav-toggle" onClick={() => setShowNav(!showNav)}>
             <svg
-              class={showNav ? "hamburger-menu active" : "hamburger-menu"}
+              className={showNav ? "hamburger-menu active" : "hamburger-menu"}
               viewBox="0 0 100 100"
               width="80"
             >
               <path
-                class="hamburger-line top"
+                className="hamburger-line top"
                 d="m 30,33 h 40 c 0,0 9.044436,-0.654587 9.044436,-8.508902 0,-7.854315 -8.024349,-11.958003 -14.89975,-10.85914 -6.875401,1.098863 -13.637059,4.171617 -13.637059,16.368042 v 40"
               />
-              <path class="hamburger-line middle" d="m 30,50 h 40" />
+              <path className="hamburger-line middle" d="m 30,50 h 40" />
               <path
-                class="hamburger-line bottom"
+                className="hamburger-line bottom"
                 d="m 30,67 h 40 c 12.796276,0 15.357889,-11.717785 15.357889,-26.851538 0,-15.133752 -4.786586,-27.274118 -16.667516,-27.274118 -11.88093,0 -18.499247,6.994427 -18.435284,17.125656 l 0.252538,40"
               />
             </svg>
@@ -33,9 +33,15 @@ const Header = ({ siteTitle }) => {
         </div>
         <div className={showNav ? "main-nav show-main-nav" : "main-nav"}>
           <ul className="main-nav-btn-container">
-            <li className="nav-link-btn">About</li>
-            <li className="nav-link-btn">Project</li>
-            <li className="nav-link-btn">Contact</li>
+            <li>
+              <button className="nav-link-btn">About</button>
+            </li>
+            <li>
+              <button className="nav-link-btn">Projects</button>
+            </li>
+            <li>
+              <button className="nav-link-btn">Contact</button>
+            </li>
           </ul>
         </div>
       </nav>
