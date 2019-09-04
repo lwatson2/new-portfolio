44de5049237a0e151b592609f8afd13094d5a8d2
lwatson2 <watson.logan@yahoo.com>
2019-09-04 17:34:05 -0500

    Started on recolor

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -27,12 +27,12 @@ const Project = () => {
             <path
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
               stroke="transparent"
-              fill="#333"
+              fill="#51D0DE"
             />
             <path
               className="card-line"
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
-              stroke="pink"
+              stroke="#1C3041"
               stroke-width="3"
               fill="transparent"
             />
@@ -63,12 +63,12 @@ const Project = () => {
             <path
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
               stroke="transparent"
-              fill="#333"
+              fill="#51D0DE"
             />
             <path
               className="card-line"
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
-              stroke="pink"
+              stroke="#1C3041"
               stroke-width="3"
               fill="transparent"
             />
@@ -100,12 +100,12 @@ const Project = () => {
             <path
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
               stroke="transparent"
-              fill="#333"
+              fill="#51D0DE"
             />
             <path
               className="card-line"
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
-              stroke="pink"
+              stroke="#1C3041"
               stroke-width="3"
               fill="transparent"
             />
@@ -136,12 +136,12 @@ const Project = () => {
             <path
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500"
               stroke="transparent"
-              fill="#333"
+              fill="#51D0DE"
             />
             <path
               className="card-line"
               d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400"
-              stroke="pink"
+              stroke="#1C3041"
               stroke-width="3"
               fill="transparent"
             />
